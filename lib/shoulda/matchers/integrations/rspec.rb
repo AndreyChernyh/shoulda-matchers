e36a196e3e875ddf8e822a2165e115afefdf2f40
Andrey Chernih <andrey.chernih@gmail.com>
2012-12-12 13:10:57 +0400

    Add rspec integration fix (thanks to @hron)

--- lib/shoulda/matchers/integrations/rspec.rb
+++ lib/shoulda/matchers/integrations/rspec.rb
@@ -1,37 +1,24 @@
 # :enddoc:
+require 'rspec'
 
-if defined?(::ActiveRecord)
-  require 'shoulda/matchers/active_record'
-  require 'shoulda/matchers/active_model'
-  module RSpec::Matchers
-    include Shoulda::Matchers::ActiveRecord
-    include Shoulda::Matchers::ActiveModel
+RSpec.configure do |config|
+  if defined?(::ActiveRecord)
+    require 'shoulda/matchers/active_record'
+    require 'shoulda/matchers/active_model'
+    config.include Shoulda::Matchers::ActiveRecord
+    config.include Shoulda::Matchers::ActiveModel
+  elsif defined?(::ActiveModel)
+    require 'shoulda/matchers/active_model'
+    config.include Shoulda::Matchers::ActiveModel
   end
-elsif defined?(::ActiveModel)
-  require 'shoulda/matchers/active_model'
-  module RSpec::Matchers
-    include Shoulda::Matchers::ActiveModel
-  end
-end
 
-if defined?(::ActionController)
-  require 'shoulda/matchers/action_controller'
-  module RSpec
-    module Rails
-      module ControllerExampleGroup
-        include Shoulda::Matchers::ActionController
-      end
-    end
+  if defined?(::ActionController)
+    require 'shoulda/matchers/action_controller'
+    config.include Shoulda::Matchers::ActionController
   end
-end
 
-if defined?(::ActionMailer)
-  require 'shoulda/matchers/action_mailer'
-  module RSpec
-    module Rails
-      module MailerExampleGroup
-        include Shoulda::Matchers::ActionMailer
-      end
-    end
+  if defined?(::ActionMailer)
+    require 'shoulda/matchers/action_mailer'
+    config.include Shoulda::Matchers::ActionMailer
   end
 end
